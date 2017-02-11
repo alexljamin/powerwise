8d83777c9ce9e516bcbbef6f7f32a83fb32e76e7
Aleksandr Ljamin <aleksandr.lmn@gmail.com>
2017-02-12 00:10:08 +1030

    initial

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+    minifyCSS = require('gulp-clean-css'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename')
+    browserSync = require('browser-sync').create();
+    del = require('del');
+
+gulp.task('clean:docs', function(){
+    del.sync('./docs/*')
+})
+
+gulp.task('browserSync', function(){
+    browserSync.init({
+        server:{
+            baseDir: './docs',
+        },  
+        port: 8080
+    })
+});
+
+gulp.task('sass', function(){
+    gulp.src('./src/*.sass')
+        .pipe(sass())
+        .pipe(minifyCSS())
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('./docs'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('copy-files', function(){
+    gulp.src("./src/*.html")
+        .pipe(gulp.dest('./docs'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+    gulp.src("./src/assets/*.*")
+        .pipe(gulp.dest('./docs/assets'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('./src/*.sass', ['sass'])
+    gulp.watch('./src/*.html', ['copy-files'])
+});
+
+gulp.task('default', ['clean:docs', 'browserSync', 'sass', 'copy-files', 'watch']);
